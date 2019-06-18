b2e61405a3f4deb9bdf96ae9c83591e85ff467b0
jordaan <jorvdmerwe@gmail.com>
2019-06-19 00:27:08 +0200

    End of section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    // http://localhost:5000/api/values
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -20,40 +21,41 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
-    // GET api/values
-    [HttpGet]
-    public async Task<IActionResult> Get()
-    {
-        var values = await _context.Values.ToListAsync(); 
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var values = await _context.Values.ToListAsync(); 
 
-        return Ok(values);
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
-
-    // GET api/values/5
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetValue(int id)
-    {
-        var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
-
-        return Ok(value);
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
-}
 }
